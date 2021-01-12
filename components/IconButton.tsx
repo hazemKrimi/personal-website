f79c18d097c4612467fad1f38ec4dc3667eebeeb
Hazem Krimi <krimihazem1@gmail.com>
2021-01-12 23:32:06 +0100

    Add width and height as props for icon button component

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -3,21 +3,29 @@ import styled from 'styled-components';
 import Image from 'next/image';
 interface Props {
 	icon: string;
+	width?: number;
+	height?: number;
 	onClick?: () => void;
 }
 
 const Btn = styled.button`
-	display: inline;
 	cursor: pointer;
 	background: none;
 	border: none;
-	color: none;
+	display: flex;
+	align-items: center;
 `;
 
-const IconButton: FC<Props & { className?: string }> = ({ icon, onClick, className }) => {
+const IconButton: FC<Props & { className?: string }> = ({
+	icon,
+	onClick,
+	className,
+	width = 24,
+	height = 24
+}) => {
 	return (
 		<Btn onClick={onClick} className={className}>
-			<Image src={icon} width={24} height={24} />
+			<Image src={icon} width={width} height={height} />
 		</Btn>
 	);
 };
