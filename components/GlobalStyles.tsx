852adc0f1c9e444ce67111e6fbf0c0a9b7d9b906
Hazem Krimi <krimihazem1@gmail.com>
2021-01-13 23:22:41 +0100

    Update global styles

--- components/GlobalStyles.tsx
+++ components/GlobalStyles.tsx
@@ -14,13 +14,20 @@ const Global = createGlobalStyle<Props>`
 		font-size: 17px;
 		line-height: 1.5;
 		outline: none;
+		user-select: text;
 		-webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+
+		&::selection {
+			background: ${({ dark, theme }) => (dark ? theme.colors.dark.text : theme.colors.light.text)};
+			color: ${({ dark, theme }) =>
+				dark ? theme.colors.dark.background : theme.colors.light.background};
+		}
   }
 
 	body {
 		background: ${({ dark, theme }) =>
 			dark ? theme.colors.dark.background : theme.colors.light.background};
-    color: ${({ dark, theme }) => (dark ? theme.colors.dark.text : theme.colors.light.text)};
+    color: ${({ dark, theme }) => (dark ? theme.colors.dark.text : theme.colors.light.text)}; 
 		transition: color 250ms ease-in-out, background 250ms ease-in-out;
 		scroll-behavior: smooth;
 	}
