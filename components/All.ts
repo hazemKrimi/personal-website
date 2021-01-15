480df17b384971fcd9492ca39ed66b3ea3199c49
Hazem Krimi <krimihazem1@gmail.com>
2021-01-15 18:56:06 +0100

    Add necessary components and embeds for content

--- components/All.ts
+++ components/All.ts
@@ -1,7 +1,16 @@
+import { CodePen, Gist, YouTube, CodeSandbox, Vimeo, Tweet } from 'mdx-embed';
 import Image from 'next/image';
+import MDXButton from './MDXButton';
 
 const All = {
-	Image
+	Button: MDXButton,
+	Image,
+	CodePen,
+	Gist,
+	Vimeo,
+	CodeSandbox,
+	Tweet,
+	YouTube
 };
 
 export default All;
