763db6f5c1c3f87ac2246b02ab12e4781810bca1
Hazem Krimi <krimihazem1@gmail.com>
2022-02-13 19:17:00 +0100

    Fix content utilities

--- utils/blog.ts
+++ utils/blog.ts
@@ -7,46 +7,54 @@ const blogPostsDirectory = path.join(process.cwd(), '_blog');
 export const getBlogPosts = () => {
 	const fileNames = fs.readdirSync(blogPostsDirectory);
 
-	const allBlogPostsData = fileNames.map(filename => {
-		const slug = filename.replace('.mdx', '');
+	if (fileNames) {
+		const allBlogPostsData = fileNames.map(filename => {
+			const slug = filename.replace('.mdx', '');
 
-		const fullPath = path.join(blogPostsDirectory, filename);
-		const fileContents = fs.readFileSync(fullPath, 'utf8');
-		const { data } = matter(fileContents);
+			const fullPath = path.join(blogPostsDirectory, filename);
+			const fileContents = fs.readFileSync(fullPath, 'utf8');
+			const { data } = matter(fileContents);
 
-		const options = { month: 'long', day: 'numeric', year: 'numeric' };
-		// @ts-ignore
-		const formattedDate = new Date(data.date).toLocaleDateString('en-IN', options);
+			const options = { month: 'long', day: 'numeric', year: 'numeric' };
+			// @ts-ignore
+			const formattedDate = new Date(data.date).toLocaleDateString('en-IN', options);
 
-		const frontmatter = {
-			...data,
-			date: formattedDate
-		};
-		return {
-			slug,
-			...frontmatter
-		};
-	});
+			const frontmatter = {
+				...data,
+				date: formattedDate
+			};
+			return {
+				slug,
+				...frontmatter
+			};
+		});
 
-	return allBlogPostsData.sort((a, b) => {
-		if (new Date(a.date) < new Date(b.date)) {
-			return 1;
-		} else {
-			return -1;
-		}
-	});
+		return allBlogPostsData.sort((a, b) => {
+			if (new Date(a.date) < new Date(b.date)) {
+				return 1;
+			} else {
+				return -1;
+			}
+		});
+	}
+
+	return [];
 };
 
 export const getBlogPostsSlugs = () => {
 	const fileNames = fs.readdirSync(blogPostsDirectory);
 
-	return fileNames.map(filename => {
-		return {
-			params: {
-				slug: filename.replace('.mdx', '')
-			}
-		};
-	});
+	if (fileNames) {
+		return fileNames.map(filename => {
+			return {
+				params: {
+					slug: filename.replace('.mdx', '')
+				}
+			};
+		});
+	}
+
+	return [];
 };
 
 export const getBlogPostdata = async (slug: string) => {
--- utils/portfolio.ts
+++ utils/portfolio.ts
@@ -7,46 +7,54 @@ const portfolioProjects = path.join(process.cwd(), '_portfolio');
 export const getPortfolioProjects = () => {
 	const fileNames = fs.readdirSync(portfolioProjects);
 
-	const allPortfolioProjectsData = fileNames.map(filename => {
-		const slug = filename.replace('.mdx', '');
+	if (fileNames) {
+		const allPortfolioProjectsData = fileNames.map(filename => {
+			const slug = filename.replace('.mdx', '');
 
-		const fullPath = path.join(portfolioProjects, filename);
-		const fileContents = fs.readFileSync(fullPath, 'utf8');
-		const { data } = matter(fileContents);
+			const fullPath = path.join(portfolioProjects, filename);
+			const fileContents = fs.readFileSync(fullPath, 'utf8');
+			const { data } = matter(fileContents);
 
-		const options = { month: 'long', day: 'numeric', year: 'numeric' };
-		// @ts-ignore
-		const formattedDate = new Date(data.date).toLocaleDateString('en-IN', options);
+			const options = { month: 'long', day: 'numeric', year: 'numeric' };
+			// @ts-ignore
+			const formattedDate = new Date(data.date).toLocaleDateString('en-IN', options);
 
-		const frontmatter = {
-			...data,
-			date: formattedDate
-		};
-		return {
-			slug,
-			...frontmatter
-		};
-	});
+			const frontmatter = {
+				...data,
+				date: formattedDate
+			};
+			return {
+				slug,
+				...frontmatter
+			};
+		});
 
-	return allPortfolioProjectsData.sort((a, b) => {
-		if (new Date(a.date) < new Date(b.date)) {
-			return 1;
-		} else {
-			return -1;
-		}
-	});
+		return allPortfolioProjectsData.sort((a, b) => {
+			if (new Date(a.date) < new Date(b.date)) {
+				return 1;
+			} else {
+				return -1;
+			}
+		});
+	}
+
+	return [];
 };
 
 export const getPortfolioPorjectsSlugs = () => {
 	const fileNames = fs.readdirSync(portfolioProjects);
 
-	return fileNames.map(filename => {
-		return {
-			params: {
-				slug: filename.replace('.mdx', '')
-			}
-		};
-	});
+	if (fileNames) {
+		return fileNames.map(filename => {
+			return {
+				params: {
+					slug: filename.replace('.mdx', '')
+				}
+			};
+		});
+	}
+
+	return [];
 };
 
 export const getPortfolioProjectdata = async (slug: string) => {
