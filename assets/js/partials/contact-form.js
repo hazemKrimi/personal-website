892ddca71a4c2cd9e9af204bd00a891de0f0d12a
Hazem Krimi <krimihazem1@gmail.com>
2023-11-08 19:07:54 +0100

    Handle form without redirect

--- assets/js/partials/contact-form.js
+++ assets/js/partials/contact-form.js
@@ -1,4 +1,32 @@
-document.querySelector('form').addEventListener('submit', (event) => {
+const form = document.querySelector('form');
+const submissionStatus = form.querySelector('#submission-status');
+
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  console.log(new FormData(event.target));
+
+  fetch(event.target.action, {
+    method: event.target.method,
+    body: new FormData(event.target),
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        submissionStatus.innerHTML = 'Message sent successfully!';
+        submissionStatus.style.display = 'block';
+        form.reset();
+      }
+    })
+    .catch((error) => {
+      submissionStatus.innerHTML = 'Error sending message!';
+      submissionStatus.style.display = 'block';
+      console.error(error);
+    })
+    .finally(() => {
+      setTimeout(() => {
+        submissionStatus.innerHTML = '';
+        submissionStatus.style.display = 'none';
+      }, 5000);
+    });
 });
